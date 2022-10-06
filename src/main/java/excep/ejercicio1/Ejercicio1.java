1644154a90b8075f2b34743bbb7fcb104480ad1c
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2022-10-06 12:26:02 +0200

    V1

---
+++ src/main/java/excep/ejercicio1/Ejercicio1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package excep.ejercicio1;
+
+/**
+ *
+ * @author Dam
+ */
+public class Ejercicio1 {
+    
+}
---
+++ src/main/java/excep/mainTest/PSPROEjerciciosCortosExcepciones.java
@@ -0,0 +1,17 @@
+package excep.mainTest;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+/**
+ *
+ * @author Dam
+ */
+public class PSPROEjerciciosCortosExcepciones {
+
+    public static void main(String[] args) {
+        
+    }
+}
